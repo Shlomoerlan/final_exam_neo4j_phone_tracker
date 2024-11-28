4c4b36ed2ef0166b8a1190048cbd256dbfb3d0b7
shlomo <shlomo.erlan@gemail.com>
2024-11-28 14:49:19 +0200

    new project moved

--- app/db/database.py
+++ app/db/database.py
@@ -4,7 +4,6 @@ from app.settings.config import NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD
 
 redis_client = redis.Redis(host="localhost",port=6379,db=0)
 
-# neo4j db
 driver = GraphDatabase.driver(
     NEO4J_URI,
     auth=(NEO4J_USER,NEO4J_PASSWORD)
